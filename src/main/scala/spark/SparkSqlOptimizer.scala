8d98f55d1378f8df081ff163ddf0c7ab25fe5be4
Zhou Zach <wander669@gmail.com>
2019-04-30 16:11:50 +0800

    SparkSqlOptimizer

---
+++ src/main/scala/spark/SparkSqlOptimizer.scala
@@ -0,0 +1,29 @@
+package spark
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.slf4j.{Logger, LoggerFactory}
+
+object SparkSqlOptimizer {
+
+  val logger: Logger = LoggerFactory.getLogger(getClass)
+
+  val sparkSession: SparkSession = SparkSession.builder.appName("Simple Application")
+    .enableHiveSupport()
+    .getOrCreate()
+
+
+  /**
+    * https://confusedcoders.com/data-engineering/spark/spark-sql-job-executing-very-slow-performance-tuning
+    */
+  def handleSlowQuery(dataFrame: DataFrame): Unit ={
+    // For handling large number of smaller files
+    val events = sparkSession.sqlContext.createDataFrame(dataFrame.rdd, dataFrame.schema).coalesce(400)
+    events.createOrReplaceTempView("input_events")
+    // For overriding default value of 200
+    sparkSession.sqlContext.sql("SET spark.sql.shuffle.partitions=10")
+
+    val sql_query =""
+    sparkSession.sqlContext.sql(sql_query)
+  }
+
+}
