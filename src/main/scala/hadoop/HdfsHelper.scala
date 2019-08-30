3bdec5c9d82c8fa1ce1cab2ac07c5d7d7e8ec3df
Zhou Zach <wander669@gmail.com>
2019-08-30 11:56:21 +0800

    add hdfs merge method

--- src/main/scala/hadoop/HdfsHelper.scala
+++ src/main/scala/hadoop/HdfsHelper.scala
@@ -4,11 +4,23 @@ import java.io.IOException
 import java.net.URI
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
 
 import scala.io.Source
 
 object HdfsHelper {
+
+  def main(args: Array[String]): Unit = {
+    val hdfsMaster = "hdfs://<dfs.nameservices>:8020"
+    val fs = HdfsHelper.getFileSystemInstance(hdfsMaster)
+
+    val csvPath = ""
+    val tagName = "test-tag"
+    HdfsHelper.merge(fs, s"$csvPath/$tagName",
+                     fs, s"$csvPath/churn-$tagName.csv",
+         true, new Configuration(), null)
+
+  }
 
   def getFileSystemInstance(masterUrl: String): FileSystem = {
     FileSystem.get(new URI(masterUrl), new Configuration())
@@ -96,11 +108,11 @@ object HdfsHelper {
   }
 
   /**
-    *
-    * @param fs
-    * @param srcFilePattern eg: /user/sqoop/part*
-    * @param dstFilePrefix
-    */
+   *
+   * @param fs
+   * @param srcFilePattern eg: /user/sqoop/part*
+   * @param dstFilePrefix
+   */
   def renameBulk(fs: FileSystem, srcFilePattern: String, dstFilePrefix: String, dstFileSuffix: String) = {
 
     val srcPath = new Path(srcFilePattern)
@@ -121,6 +133,37 @@ object HdfsHelper {
   def close(fs: FileSystem) = {
     try {
       fs.close()
+    } catch {
+      case e: IOException =>
+        e.printStackTrace()
+    }
+
+  }
+
+  /**
+   *
+   * is faster 36 times than spark repartition(1)
+   * but is Incompatible with hadoop 3.0.0
+   * https://stackoverflow.com/questions/42035735/how-to-do-copymerge-in-hadoop-3-0
+   *
+   * @param srcFS
+   * @param srcDir  多个文件的父目录
+   * @param dstFS
+   * @param dstFile 目标文件
+   * @param deleteSource
+   * @param conf
+   * @param addString
+   * @return
+   */
+  def merge(srcFS: FileSystem, srcDir: String,
+            dstFS: FileSystem, dstFile: String,
+            deleteSource: Boolean,
+            conf: Configuration, addString: String) = {
+    try {
+
+      FileUtil.copyMerge(srcFS, new Path(srcDir),
+        dstFS, new Path(dstFile),
+        true, new Configuration(), null)
     } catch {
       case e: IOException =>
         e.printStackTrace()
--- src/main/scala/spark/sql/CaseWhen.scala
+++ src/main/scala/spark/sql/CaseWhen.scala
@@ -10,14 +10,13 @@ object CaseWhen {
     val orders = Seq(
       ("o1", "u1", "2019-07-01", 100.0, 1),
       ("o2", "u1", "2019-07-22", 120.0, 2),
-      ("o3", "u2", "2019-08-01", 100.0, 3),
-      ("o4", "u1", "2019-08-02", 220.0, 4),
+      ("o4", "u1", "2019-08-02", 220.0, -4)
 
 
-      ("o5", "u2", "2019-09-12", 220.0, 5)
 
     )
-    val orderDF = sparkSession.createDataFrame(orders).toDF("order_id", "member_id", "order_time", "price", "qty")
+    val orderDF = sparkSession.createDataFrame(orders)
+      .toDF("order_id", "member_id", "order_time", "price", "qty")
     orderDF.createOrReplaceTempView("orders")
 
 
@@ -27,6 +26,16 @@ object CaseWhen {
         | select member_id, case when sum(price) > 0 and sum(qty)>0  then 1 else 0 end as flag
         | from orders
         | group by member_id
+        |
+        |
+      """.stripMargin)
+      .show()
+
+    sparkSession.sql(
+      """
+        |
+        | select case when (price + qty) is not null  then price + qty else 0 end as flag
+        | from orders
         |
         |
       """.stripMargin)
