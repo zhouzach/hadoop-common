917bce84981241751002efc90a6b4331c623590e
Zach Zhou <769087026@qq.com>
2019-10-10 15:08:36 +0800

    complete MonitorStarter

--- src/main/scala/spark/monitor/MonitorStarter.scala
+++ src/main/scala/spark/monitor/MonitorStarter.scala
@@ -1,8 +1,14 @@
 package spark.monitor
 
 import org.apache.spark.sql.SparkSession
+import org.joda.time.DateTime
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.util.{Failure, Success, Try}
 
 object MonitorStarter {
+
+  protected val logger: Logger = LoggerFactory.getLogger(getClass)
 
   implicit val spark: SparkSession = SparkSession.builder
     .config("enableSendEmailOnTaskFail", "true")
@@ -12,19 +18,36 @@ object MonitorStarter {
     .getOrCreate()
 
   def main(args: Array[String]): Unit = {
-    //    val userData = Seq(("Leo", 16, 90), ("Marry", 21, 100), ("Jack", 14, 85), ("Tom", 16, 35), ("", 12, 62), (null, 22, 65))
-    //    val dataFrame = spark.createDataFrame(userData).toDF("name", "age", "score")
-    //    dataFrame.createOrReplaceTempView("users")
-    //
-    //    spark.sql(
-    //      """
-    //        |
-    //        |select di from users
-    //        |""".stripMargin).show()
+    Try{
+        val userData = Seq(("Leo", 16, 90), ("Marry", 21, 100), ("Jack", 14, 85), ("Tom", 16, 35), ("", 12, 62), (null, 22, 65))
+        val dataFrame = spark.createDataFrame(userData).toDF("name", "age", "score")
+        dataFrame.createOrReplaceTempView("users")
 
-    val rdd = spark.sparkContext.parallelize(Seq(1, 2, 0, 3))
-    println(rdd.getNumPartitions)
-    rdd.foreach(d => 2 / d)
+        spark.sql(
+          """
+            |
+            |select di from users
+            |""".stripMargin).show()
+    } match {
+      case Failure(ex) =>
+        logger.error("task fail: ", ex)
+        val end = DateTime.now().toString("yyyy-MM-dd HH:mm:ss.sss")
+        MailHelper.send( new MailInfo (
+          to = "769087026@qq.com":: Nil,
+          subject = s"spark job monitor on $end",
+          message = ex.getLocalizedMessage
+        ))
+
+        println("task fail: " + s", on $end")
+        println(s"error msg: ${ex.getMessage}")
+
+      case Success(uint) =>
+        val end = DateTime.now().toString("yyyy-MM-dd HH:mm:ss.sss")
+        println(s"task finished: " + s", on $end")
+    }
+//    val rdd = spark.sparkContext.parallelize(Seq(1, 2, 0, 3))
+//    println(rdd.getNumPartitions)
+//    rdd.foreach(d => 2 / d)
   }
 
 }
